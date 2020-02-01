e34379fc15f41d44e1dc8073fe27ca464e420624
Fadi Nassri <30464695+fadinassri@users.noreply.github.com>
2020-02-01 13:16:38 -0400

    Initial commit

---
+++ AutoMapperExample/Program.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMapperExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step1: Initialize the mapper
+        
+
+            //Step2: Create the source object
+            Employee emp = new Employee();
+            emp.Name = "James";
+            emp.Salary = 20000;
+            emp.Address = "London";
+            emp.Department = "IT";
+
+         
+
+            //Step3: use the mapper to map the source and destination object
+            Mapper map = new Mapper( new MapperConfiguration(cfg=> cfg.CreateMap<Employee, EmployeeDTO>()));
+            var empDTO = map.Map<Employee, EmployeeDTO>(emp);
+            //OR
+            //var empDTO = map.Map<EmployeeDTO>(emp);
+
+            Console.WriteLine("Name:" + empDTO.Name + ", Salary:" + empDTO.Salary + ", Address:" + empDTO.Address + ", Department:" + empDTO.Department);
+            Console.ReadLine();
+        }
+    }
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+    }
+
+    public class EmployeeDTO
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+    }
+}
